4186a9406e27d7f4166378edabd868ee66f088bf
Levi Ceccato <levi.ceccato@gmail.com>
2025-06-25 12:54:12 +1000

    Clean up main config and add vercel blog storage config

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,6 +1,5 @@
-// storage-adapter-import-placeholder
+import { vercelBlobStorage } from '@payloadcms/storage-vercel-blob'
 import { sqliteAdapter } from '@payloadcms/db-sqlite'
-import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import path from 'path'
 import { buildConfig } from 'payload'
@@ -15,28 +14,33 @@ const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
 export default buildConfig({
+  secret: process.env.PAYLOAD_SECRET ?? '',
+  editor: lexicalEditor(),
+  sharp,
+  typescript: {
+    outputFile: path.resolve(dirname, 'payload-types.ts'),
+  },
+  db: sqliteAdapter({
+    client: {
+      authToken: process.env.TURSO_AUTH_TOKEN,
+      url: process.env.TURSO_DATABASE_URL ?? '',
+    },
+  }),
   admin: {
     user: Users.slug,
     importMap: {
       baseDir: path.resolve(dirname),
     },
   },
-  collections: [Users, Media],
-  editor: lexicalEditor(),
-  secret: process.env.PAYLOAD_SECRET || '',
-  typescript: {
-    outputFile: path.resolve(dirname, 'payload-types.ts'),
-  },
-  globals: [Nav],
-  db: sqliteAdapter({
-    client: {
-      authToken: process.env.TURSO_AUTH_TOKEN ?? '',
-      url: process.env.TURSO_DATABASE_URL ?? '',
-    },
-  }),
-  sharp,
   plugins: [
-    payloadCloudPlugin(),
-    // storage-adapter-placeholder
+    vercelBlobStorage({
+      enabled: process.env.ENABLE_BLOB_STORAGE === 'true',
+      token: process.env.BLOB_READ_WRITE_TOKEN,
+      collections: {
+        media: true,
+      },
+    }),
   ],
+  globals: [Nav],
+  collections: [Users, Media],
 })
