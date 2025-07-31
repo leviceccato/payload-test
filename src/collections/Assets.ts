4ea1044c18745a79f25f3e1a6f50ceb6848c6dc8
Levi Ceccato <levi.ceccato@gmail.com>
2025-07-31 13:45:28 +1000

    Use CSS to contain image

--- src/collections/Assets.ts
+++ src/collections/Assets.ts
@@ -6,24 +6,8 @@ export const Assets = {
   folders: true,
   upload: {
     adminThumbnail: ({ doc }) => {
-      if (typeof doc.filename !== 'string') {
-        return null
-      }
-
-      const filenameSegments = doc.filename.split('.')
-      const extension = filenameSegments.pop()
-      const filename = filenameSegments.join('.')
-    
-      return `https://gsti4renzefw2pst.public.blob.vercel-storage.com/${getDatabaseName()}/${filename}-100x100.${extension}`
+      return `https://gsti4renzefw2pst.public.blob.vercel-storage.com/${getDatabaseName()}/${doc.filename}`
     },
-    imageSizes: [
-      {
-        name: 'admin',
-        fit: 'contain',
-        width: 100,
-        height: 100,
-      },
-    ],
   },
   access: {
     read: () => true,
